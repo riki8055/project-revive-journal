1db60e7da3746306aee70a55b81b560f7a2daa30
Ritik Thakur <thakurritik8055@gmail.com>
2025-12-25 14:33:35 +0530

    guard against missing command (error handling)

--- weekly-projects/1-expense-tracker/index.js
+++ weekly-projects/1-expense-tracker/index.js
@@ -1,6 +1,11 @@
 const args = process.argv.slice(2);
 
 const command = args[0];
+if (!command) {
+  console.error("❌ No command provided!");
+  process.exit(1);
+}
+
 const commandArgs = args.slice(1);
 
 console.log("Command: ", command);

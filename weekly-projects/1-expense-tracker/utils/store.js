315dd81962387f292602705924a94b730dd1daac
Ritik Thakur <64410199+riki8055@users.noreply.github.com>
2025-12-26 10:27:30 +0000

    implement the store

---
+++ weekly-projects/1-expense-tracker/utils/store.js
@@ -0,0 +1,30 @@
+function createExpenseStore() {
+  const expenses = [];
+
+  function add(expense) {
+    expenses.push(expense);
+  }
+
+  function getAll() {
+    return [...expenses];
+  }
+
+  function removeById(id) {
+    const index = expenses.findIndex((e) => e.id === id);
+    //The findIndex() method returns the index of the
+    // first item for which callback returned true.
+
+    if (index === -1) {
+      return false;
+    }
+
+    expenses.splice(index, 1);
+    // The number of elements to remove.
+    // Splice() removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements
+    return true;
+  }
+
+  return { add, getAll, removeById };
+}
+
+module.exports = { createExpenseStore };

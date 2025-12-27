43e9629daa8a2bd5ecf244fa9ba1e8455c04c46a
Ritik Thakur <64410199+riki8055@users.noreply.github.com>
2025-12-27 05:53:57 +0000

    implement loadExpenses

--- weekly-projects/1-expense-tracker/utils/storage.js
+++ weekly-projects/1-expense-tracker/utils/storage.js
@@ -2,3 +2,22 @@ const fs = require("fs/promises");
 const path = require("path");
 
 const DATA_FILE = path.join(__dirname, "../expenses.json");
+
+async function loadExpenses() {
+  try {
+    const data = await fs.readFile(DATA_FILE, "utf-8");
+    const parsed = JSON.parse(data);
+
+    if (!Array.isArray(parsed)) {
+      throw new Error("Invalid data format!");
+    }
+
+    return parsed;
+  } catch (error) {
+    // File not found → first run → no expenses
+    if (error.code === "ENOENT") {
+      return [];
+    }
+    throw error;
+  }
+}

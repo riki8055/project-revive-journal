64a86d328e7fab0b7ca8d108d501b22c9cfeb6af
Ritik Thakur <thakurritik8055@gmail.com>
2026-01-22 20:48:44 +0530

    update GET route (/notes), add POST route (/note)

--- weekly-projects/5_6-note-taking-app-crud/backend/router.js
+++ weekly-projects/5_6-note-taking-app-crud/backend/router.js
@@ -1,3 +1,5 @@
+const { createNote, getNotes } = require("./services/notes.service");
+
 function router(req, res) {
   const { method, url } = req;
 
@@ -8,9 +10,26 @@ function router(req, res) {
   }
 
   if (method === "GET" && url === "/notes") {
+    const notes = getNotes();
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify([]));
+    res.end(JSON.stringify(notes));
     return;
+  }
+
+  if (method === "POST" && url === "/notes") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const parsed = JSON.parse(body); // intentionally unsafe (for now)
+      const note = createNote(parsed);
+
+      res.writeHead(201, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(note));
+    });
   }
 
   // fallback

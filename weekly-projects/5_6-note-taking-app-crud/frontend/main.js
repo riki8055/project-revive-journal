fd6d29cf04a59efab0f012e80af79ef89b073526
Ritik Thakur <thakurritik8055@gmail.com>
2026-01-25 21:05:47 +0530

    modify main.js

--- weekly-projects/5_6-note-taking-app-crud/frontend/main.js
+++ weekly-projects/5_6-note-taking-app-crud/frontend/main.js
@@ -1,5 +1,20 @@
 import { initEvents } from "./events/notes.events.js";
 import { renderNotes } from "./ui/notes.ui.js";
+import { setNotes } from "./state/notes.state.js";
+import { fetchNotes } from "./api/notes.api.js";
 
-initEvents();
-renderNotes();
+async function initApp() {
+  try {
+    const notes = await fetchNotes();
+
+    setNotes(notes);
+    renderNotes();
+  } catch (error) {
+    alert("Failed to load notes");
+    console.error(error);
+  }
+
+  initEvents();
+}
+
+initApp();

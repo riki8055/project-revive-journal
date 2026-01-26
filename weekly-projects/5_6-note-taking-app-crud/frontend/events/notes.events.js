c48caac286184f8f27ecd958d836c9e3e7c4c503
Ritik Thakur <thakurritik8055@gmail.com>
2026-01-26 08:21:37 +0530

    validate form before calling backend (frontend)

--- weekly-projects/5_6-note-taking-app-crud/frontend/events/notes.events.js
+++ weekly-projects/5_6-note-taking-app-crud/frontend/events/notes.events.js
@@ -11,6 +11,11 @@ function initEvents() {
     const title = document.getElementById("title").value;
     const content = document.getElementById("content").value;
 
+    if (!title.trim() || !content.trim()) {
+      alert("Title and content are required");
+      return;
+    }
+
     try {
       const createdNote = await createNote({ title, content });
 

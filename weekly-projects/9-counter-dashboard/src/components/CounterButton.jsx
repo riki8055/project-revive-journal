ab81c26106e61bdac83ca8f8bd4ccdd8a8e6ecdb
Ritik Thakur <thakurritik8055@gmail.com>
2026-02-18 21:30:11 +0530

    memoize CounterButton component

--- weekly-projects/9-counter-dashboard/src/components/CounterButton.jsx
+++ weekly-projects/9-counter-dashboard/src/components/CounterButton.jsx
@@ -1,5 +1,7 @@
-export default function CounterButton({ onIncrement, index }) {
+import React from "react";
+
+export default React.memo(function CounterButton({ increment, index }) {
   console.log("CounterButton rendered:", index);
 
-  return <button onClick={onIncrement}>Increment</button>;
-}
+  return <button onClick={() => increment(index)}>Increment</button>;
+});
--- weekly-projects/9-counter-dashboard/src/components/CounterCard.jsx
+++ weekly-projects/9-counter-dashboard/src/components/CounterCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import CounterButton from "./CounterButton";
 
-export default React.memo(function CounterCard({ value, onIncrement, index }) {
+export default React.memo(function CounterCard({ value, increment, index }) {
   console.log("CounterCard rendered:", index);
 
   return (
     <div style={{ margin: "10px", border: "1px solid gray", padding: "10px" }}>
       <h3>Counter {index}</h3>
       <p>Value: {value}</p>
-      <CounterButton index={index} onIncrement={onIncrement} />
+      <CounterButton index={index} increment={increment} />
     </div>
   );
 });

859375093a9397da61b2497383c61ee85a43eab1
Ritik Thakur <thakurritik8055@gmail.com>
2026-02-24 22:04:44 +0530

    apply the correct architecture

--- weekly-learnings/week-10/src/components/Cart.jsx
+++ weekly-learnings/week-10/src/components/Cart.jsx
@@ -2,12 +2,6 @@ import { useState, useEffect } from "react";
 
 export default function Cart() {
   const [cartItems, setCartItems] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
-
-  useEffect(() => {
-    const total = cartItems.reduce((sum, item) => sum + item.price, 0);
-    setTotalPrice(total);
-  }, [cartItems]);
 
   const addItem = () => {
     const newItem = {
@@ -17,17 +11,14 @@ export default function Cart() {
 
     const updatedCart = [...cartItems, newItem];
     setCartItems(updatedCart);
-
-    // Manually sync total
-    // setTotalPrice(totalPrice + newItem.price);
   };
 
   const removeItem = (id) => {
     const updatedCart = cartItems.filter((item) => item.id !== id);
     setCartItems(updatedCart);
-
-    // ❌ Intentionally forget to update totalPrice
   };
+
+  const totalPrice = cartItems.reduce((sum, item) => sum + item.price, 0);
 
   return (
     <div>

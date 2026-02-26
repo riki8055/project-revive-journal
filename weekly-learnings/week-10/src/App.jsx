74ee67135221102bd834a8f335948592d55c90dd
Ritik Thakur <thakurritik8055@gmail.com>
2026-02-26 06:52:57 +0530

    create BadUseEffect component and create an infinite loop

--- weekly-learnings/week-10/src/App.jsx
+++ weekly-learnings/week-10/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import Counter from "./components/Counter";
 import Cart from "./components/Cart";
+import InfiniteLoop from "./components/BadUseEffect";
 
 function App() {
-  return <Cart />;
+  return <InfiniteLoop />;
 }
 
 export default App;
---
+++ weekly-learnings/week-10/src/components/BadUseEffect.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function InfiniteLoop() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("Effect running...");
+    setCount(count + 1);
+  });
+
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  );
+}

d6bbfc0ca8c547fc8cab95138d791418932423be
Ritik Thakur <thakurritik8055@gmail.com>
2026-03-04 19:23:19 +0530

    UI fix

--- weekly-projects/11-multi-step-form/src/components/MultiStepForm/StepPersonal.jsx
+++ weekly-projects/11-multi-step-form/src/components/MultiStepForm/StepPersonal.jsx
@@ -31,38 +31,32 @@ export default function StepPersonal({ data, dispatch, errors }) {
 
   return (
     <>
-      <div>
-        <input
-          type="text"
-          name="firstName"
-          value={data.firstName}
-          onChange={handleChange}
-          placeholder="First Name"
-        />
-        {errors.firstName && <p>{errors.firstName}</p>}
-      </div>
+      <input
+        type="text"
+        name="firstName"
+        value={data.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      />
+      {errors.firstName && <p>{errors.firstName}</p>}
 
-      <div>
-        <input
-          type="text"
-          name="lastName"
-          value={data.lastName}
-          onChange={handleChange}
-          placeholder="Last Name"
-        />
-        {errors.lastName && <p>{errors.lastName}</p>}
-      </div>
+      <input
+        type="text"
+        name="lastName"
+        value={data.lastName}
+        onChange={handleChange}
+        placeholder="Last Name"
+      />
+      {errors.lastName && <p>{errors.lastName}</p>}
 
-      <div>
-        <input
-          type="email"
-          name="email"
-          value={data.email}
-          onChange={handleChange}
-          placeholder="Email"
-        />
-        {errors.email && <p>{errors.email}</p>}
-      </div>
+      <input
+        type="email"
+        name="email"
+        value={data.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      {errors.email && <p>{errors.email}</p>}
     </>
   );
 }

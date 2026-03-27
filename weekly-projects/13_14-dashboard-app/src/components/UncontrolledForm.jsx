4b7482f25583d2182767bd05fff79b984f981378
Ritik Thakur <thakurritik8055@gmail.com>
2026-03-27 06:34:09 +0530

    add more fields to uncontrolled form component

--- weekly-projects/13_14-dashboard-app/src/components/UncontrolledForm.jsx
+++ weekly-projects/13_14-dashboard-app/src/components/UncontrolledForm.jsx
@@ -2,14 +2,27 @@ import { useRef } from "react";
 
 export default function UncontrolledForm() {
   const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   function handleSubmit() {
-    console.log(nameRef.current.value);
+    const formData = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+
+    console.log(formData);
   }
 
   return (
     <div>
-      <input ref={nameRef} placeholder="Enter name" />
+      <h2>Uncontrolled Form</h2>
+
+      <input ref={nameRef} placeholder="Name" defaultValue="Ritik" />
+      <input ref={emailRef} placeholder="Email" />
+      <input ref={passwordRef} placeholder="Password" />
+
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );

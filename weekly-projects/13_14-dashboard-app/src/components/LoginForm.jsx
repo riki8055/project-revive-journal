3b50a3cb07d7f2681f2800426f879797fc428b20
Ritik Thakur <thakurritik8055@gmail.com>
2026-03-27 07:08:13 +0530

    add a log

--- weekly-projects/13_14-dashboard-app/src/components/LoginForm.jsx
+++ weekly-projects/13_14-dashboard-app/src/components/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 export default function LoginForm() {
+  console.log("LoginForm re-rendered");
   const [form, setForm] = useState({
     email: "",
     password: "",
